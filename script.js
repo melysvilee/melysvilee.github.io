932be8126b33f16efc57ec62e9dfc6b187935dc6
Mely <146790383+melysvilee@users.noreply.github.com>
2025-07-29 14:06:21 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function concat() {
+  var word1 = Number(document.getElementById("input1").value);
+  var word2 = Number(document.getElementById("input2").value);
+  var phraseStart = "There are many ";
+  var phraseStart2 = " ways to choose a";
+  var fullSentence = phraseStart + word1 + phraseStart2 + word2;
+  
+  document.getElementById("result").innerText = "Result: " + result;
+}
