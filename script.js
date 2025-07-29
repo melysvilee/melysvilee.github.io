dc893538a42ef3a8a4a681a5308254467749f365
Mely <146790383+melysvilee@users.noreply.github.com>
2025-07-29 14:57:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function concat() {
-  var word1 = Number(document.getElementById("input1").value);
-  var word2 = Number(document.getElementById("input2").value);
+  var word1 = String(document.getElementById("input1").value);
+  var word2 = String(document.getElementById("input2").value);
   var phraseStart = "There are many ";
   var phraseStart2 = " ways to choose a";
   var fullSentence = phraseStart + word1 + phraseStart2 + word2;
