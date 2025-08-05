b708e56bd454dadf382d500e166be8f99c4a5614
Mely <146790383+melysvilee@users.noreply.github.com>
2025-08-05 13:59:02 -0400

    Update portscript.js

--- kkcf/portfolio/portscript.js
+++ kkcf/portfolio/portscript.js
@@ -2,5 +2,21 @@
 
     function toggleDark() {
   var element = document.body;
-  element.classList.toggle("dark-mode");
+        if (document.body.style.backgroundColor === "white") {
+    // If dark mode is on, switch to light mode
+    document.body.style.backgroundColor = "dark";       // background to light
+    document.body.style.color = "white"; 
+    nav.data-bs-theme = "dark";// text to dark
+    button.textContent = "White Mode";                      // button text changes to "dark mode"
+
+    //#5
+  } else {
+    // If light mode is on, switch to dark mode
+    document.body.style.backgroundColor = "white";        // background to dark
+    document.body.style.color = "black"; 
+    av.data-bs-theme = "light";
+
+    button.textContent = "Dark Mode";                      // button text changes to "light mode"
+  }
+
 }
