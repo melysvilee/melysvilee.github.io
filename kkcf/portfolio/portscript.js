0a3204ea37824a679e6f08902ec4f62c603e1f84
Mely <146790383+melysvilee@users.noreply.github.com>
2025-08-06 11:28:28 -0400

    Update portscript.js

--- kkcf/portfolio/portscript.js
+++ kkcf/portfolio/portscript.js
@@ -4,16 +4,3 @@
   var element = document.body;
 element.classList.toggle("dark-mode");
 }
-
-const triggerEl = document.querySelector('#myTab button[data-bs-target="#profile"]')
-bootstrap.Tab.getInstance(triggerEl).show()
-
-const triggerTabList = document.querySelectorAll('#myTab button')
-triggerTabList.forEach(triggerEl => {
-  const tabTrigger = new bootstrap.Tab(triggerEl)
-
-  triggerEl.addEventListener('click', event => {
-    event.preventDefault()
-    tabTrigger.show()
-  })
-})
