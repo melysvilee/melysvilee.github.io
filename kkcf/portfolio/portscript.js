9b9800c95a6c476e3748dcfaec6a9bc4bab8edb3
Mely <146790383+melysvilee@users.noreply.github.com>
2025-08-06 10:34:58 -0400

    Update portscript.js

--- kkcf/portfolio/portscript.js
+++ kkcf/portfolio/portscript.js
@@ -4,9 +4,10 @@
   var element = document.body;
 element.classList.toggle("dark-mode");
 }
+
 <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
-    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Home</button>
+    <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">baka</button>
   </li>
   <li class="nav-item" role="presentation">
     <button class="nav-link" id="profile-tab" data-bs-toggle="tab" data-bs-target="#profile-tab-pane" type="button" role="tab" aria-controls="profile-tab-pane" aria-selected="false">Profile</button>
