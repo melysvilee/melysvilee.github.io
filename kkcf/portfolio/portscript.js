78cfcb45f14b7ba3a0aad0a4e8b01ef1919d6b4f
Mely <146790383+melysvilee@users.noreply.github.com>
2025-08-10 18:23:50 -0400

    Update portscript.js

--- kkcf/portfolio/portscript.js
+++ kkcf/portfolio/portscript.js
@@ -4,3 +4,19 @@
   var element = document.body;
 element.classList.toggle("dark-mode");
 }
+
+const test = document.getElementById("test");
+
+test.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+
+    // reset the color after a short delay
+    setTimeout(() => {
+      event.target.style.color = "";
+    }, 500);
+  },
+  false,
+);
