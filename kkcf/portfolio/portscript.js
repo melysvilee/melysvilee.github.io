7db8e85b8ce1dab3701c250f979daa9920380157
Mely <146790383+melysvilee@users.noreply.github.com>
2025-08-12 11:37:03 -0400

    Update portscript.js

--- kkcf/portfolio/portscript.js
+++ kkcf/portfolio/portscript.js
@@ -19,5 +19,6 @@ listItems.forEach((item) => {
   item.addEventListener("click", toggleDone);
 });
 
-const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
-const dropdownList = [...dropdownElementList].map(dropdownToggleEl => new bootstrap.Dropdown(dropdownToggleEl))
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
